30e560d00fa632fbe3262dbc04fb5c055fd769d6
Frédéric Bardolle <seiteta@users.noreply.github.com>
2017-10-24 17:44:46 +0200

    Remove useless code

--- backend/__init__.py
+++ backend/__init__.py
@@ -11,84 +11,6 @@ api = Api(app)
 
 parser = reqparse.RequestParser()
 
-class WorkList(Resource):
-
-    def get(self):
-        print("Call for: GET /works")
-        url = config.es_base_url['works']+'/_search'
-        query = {
-            "query": {
-                "match_all": {}
-            },
-            "size": 100
-        }
-        resp = requests.post(url, data=json.dumps(query))
-        data = resp.json()
-        works = []
-        for hit in data['hits']['hits']:
-            work = hit['_source']
-            work['id'] = hit['_id']
-            works.append(work)
-        return works
-
-    def post(self):
-        print("Call for: POST /works")
-        parser.add_argument('report')
-        parser.add_argument('recipient')
-        parser.add_argument('date')
-        parser.add_argument('team')
-        parser.add_argument('juridiction', action='append')
-        work = parser.parse_args()
-        print(work)
-        url = config.es_base_url['works']
-        resp = requests.post(url, data=json.dumps(work))
-        data = resp.json()
-        return data
-
-class Work(Resource):
-
-    def get(self, work_id):
-        print("Call for: GET /works/%s" % work_id)
-        url = config.es_base_url['works']+'/'+work_id
-        resp = requests.get(url)
-        data = resp.json()
-        work = data['_source']
-        return work
-
-    def put(self, work_id):
-        """TODO: update functionality not implemented yet."""
-        pass
-
-    def delete(self, work_id):
-        print("Call for: DELETE /works/%s" % work_id)
-        url = config.es_base_url['works']+'/'+work_id
-        resp = requests.delete(url)
-        data = resp.json()
-        return data
-
-class Style(Resource):
-    pass
-
-class StyleList(Resource):
-
-    def get(self):
-        print("Call for /juridiction")
-        url = config.es_base_url['juridiction']+'/_search'
-        query = {
-            "query": {
-                "match_all": {}
-            },
-            "size": 100
-        }
-        resp = requests.post(url, data=json.dumps(query))
-        data = resp.json()
-        juridiction = []
-        for hit in data['hits']['hits']:
-            style = hit['_source']
-            style['id'] = hit['_id']
-            juridiction.append(style)
-        return juridiction
-
 class Search(Resource):
 
     def get(self):
@@ -117,7 +39,4 @@ class Search(Resource):
             works.append(work)
         return works
 
-api.add_resource(Work, config.api_base_url+'/works/<work_id>')
-api.add_resource(WorkList, config.api_base_url+'/works')
-api.add_resource(StyleList, config.api_base_url+'/juridiction')
 api.add_resource(Search, config.api_base_url+'/search')
--- frontend/app.js
+++ frontend/app.js
@@ -2,28 +2,7 @@ var myApp = angular.module("myApp", ["ngRoute", "ngResource", "myApp.services"])
 
 var services = angular.module("myApp.services", ["ngResource"])
 services
-// .factory('Work', function($resource) {
-//     return $resource('http://localhost:5000/api/v1/works/:id', {id: '@id'}, {
-//         get: { method: 'GET' },
-//         delete: { method: 'DELETE' }
-//     });
-// })
-// .factory('Works', function($resource) {
-//     return $resource('http://localhost:5000/api/v1/works', {}, {
-//         query: { method: 'GET', isArray: true },
-//         create: { method: 'POST', }
-//     });
-// })
-// .factory('Juridiction', function($resource) {
-//     return $resource('http://localhost:5000/api/v1/juridiction/:id', {id: '@id'}, {
-//         get: { method: 'GET' }
-//     });
-// })
-// .factory('Style', function($resource) {
-//     return $resource('http://localhost:5000/api/v1/juridiction', {}, {
-//         query: { method: 'GET', isArray: true}
-//     });
-// })
+
 .factory('Search', function($resource) {
     return $resource('http://localhost:5000/api/v1/search', {q: '@q'}, {
         query: { method: 'GET', isArray: true}
@@ -36,31 +15,7 @@ myApp.config(function($routeProvider) {
         templateUrl: 'pages/main.html',
         controller: 'mainController'
     })
-    .when('/newWork', {
-        templateUrl: 'pages/work_new.html',
-        controller: 'newWorkController'
-    })
-    .when('/works', {
-        templateUrl: 'pages/works.html',
-        controller: 'workListController'
-    })
-    .when('/works/:id', {
-        templateUrl: 'pages/work_details.html',
-        controller: 'workDetailsController'
-    })
 });
-
-// myApp.filter('filterJuridiction', function() {
-//   return function(input) {
-//     var output = new Array();
-//     for (i=0; i<input.length; i++) {
-//         if (input[i].checked == true) {
-//             output.push(input[i].report);
-//         }
-//     }
-//     return output;
-//   }
-// });
 
 myApp.controller(
     'mainController',
@@ -73,48 +28,3 @@ myApp.controller(
         };
     }
 );
-
-// myApp.controller(
-//     'newWorkController',
-//     function ($scope, Juridiction, Works, $location, $timeout, $filter) {
-//         $scope.juridiction = Juridiction.query();
-//         $scope.insertWork = function () {
-//             $scope.work.juridiction = $filter('filterJuridiction')($scope.juridiction);
-//             Works.create($scope.work);
-//             $timeout(function (){
-//                 $location.path('/works').search({'created': $scope.work.report});
-//             }, 500);
-//         };
-//         $scope.cancel = function() {
-//             $location.path('/works');
-//         };
-//     }
-//
-// );
-
-// myApp.controller(
-//     'workListController',
-//     function ($scope, Works, Work, $location, $timeout) {
-//         if ($location.search().hasOwnProperty('created')) {
-//             $scope.created = $location.search()['created'];
-//         }
-//         if ($location.search().hasOwnProperty('deleted')) {
-//             $scope.deleted = $location.search()['deleted'];
-//         }
-//         $scope.deleteWork = function(work_id) {
-//             var deleted = Work.delete({id: work_id});
-//             $timeout(function(){
-//                 $location.path('/works').search({'deleted': 1})
-//             }, 500);
-//             //$scope.works = Works.query();
-//         };
-//         $scope.works = Works.query();
-//     }
-// );
-//
-// myApp.controller(
-//     'workDetailsController', ['$scope', 'Work', '$routeParams',
-//     function ($scope, Work, $routeParams) {
-//         $scope.work = Work.get({id: $routeParams.id});
-//     }
-// ]);
